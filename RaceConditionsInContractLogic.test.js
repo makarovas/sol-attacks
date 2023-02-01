73f63f466b5abad8e771aa254599b6446a496aa6
makarovas <makarofalex@gmail.com>
2023-02-01 11:02:39 +0200

    code format

--- RaceConditionsInContractLogic.test.js
+++ RaceConditionsInContractLogic.test.js
@@ -1,56 +1,57 @@
-var Vulnerable = artifacts.require("/Users/alexmakarov/projs/solidity/attacks/solidity-vulnerabilities/RaceConditionsInContractLogic.sol");
+var Vulnerable = artifacts.require(
+  "/Users/alexmakarov/projs/solidity/attacks/solidity-vulnerabilities/RaceConditionsInContractLogic.sol"
+);
 
-contract('Vulnerable', (accounts) => {
-    var creatorAddress = accounts[0];
-    var firstOwnerAddress = accounts[1];
-    var secondOwnerAddress = accounts[2];
-    var externalAddress = accounts[3];
-    var unprivilegedAddress = accounts[4]
-    /* create named accounts for contract roles */
+contract("Vulnerable", (accounts) => {
+  var creatorAddress = accounts[0];
+  var firstOwnerAddress = accounts[1];
+  var secondOwnerAddress = accounts[2];
+  var externalAddress = accounts[3];
+  var unprivilegedAddress = accounts[4];
+  /* create named accounts for contract roles */
 
-    before(async () => {
-        /* before tests */
-    })
-    
-    beforeEach(async () => {
-        /* before each context */
-    })
+  before(async () => {
+    /* before tests */
+  });
 
-    it('should revert if ...', () => {
-        return Vulnerable.deployed()
-            .then(instance => {
-                return instance.publicOrExternalContractMethod(argument1, argument2, {from:externalAddress});
-            })
-            .then(result => {
-                assert.fail();
-            })
-            .catch(error => {
-                assert.notEqual(error.message, "assert.fail()", "Reason ...");
-            });
+  beforeEach(async () => {
+    /* before each context */
+  });
+
+  it("should revert if ...", () => {
+    return Vulnerable.deployed()
+      .then((instance) => {
+        return instance.publicOrExternalContractMethod(argument1, argument2, {
+          from: externalAddress,
         });
+      })
+      .then((result) => {
+        assert.fail();
+      })
+      .catch((error) => {
+        assert.notEqual(error.message, "assert.fail()", "Reason ...");
+      });
+  });
 
-    context('testgroup - security tests - description...', () => {
-        //deploy a new contract
-        before(async () => {
-            /* before tests */
-            const newVulnerable =  await Vulnerable.new()
-        })
-        
+  context("testgroup - security tests - description...", () => {
+    //deploy a new contract
+    before(async () => {
+      /* before tests */
+      const newVulnerable = await Vulnerable.new();
+    });
 
-        beforeEach(async () => {
-            /* before each tests */
-        })
+    beforeEach(async () => {
+      /* before each tests */
+    });
 
-        
+    it("fails on initialize ...", async () => {
+      return assertRevert(async () => {
+        await newVulnerable.initialize();
+      });
+    });
 
-        it('fails on initialize ...', async () => {
-            return assertRevert(async () => {
-                await newVulnerable.initialize()
-            })
-        })
-
-        it('checks if method returns true', async () => {
-            assert.isTrue(await newVulnerable.thisMethodShouldReturnTrue())
-        })
-    })
+    it("checks if method returns true", async () => {
+      assert.isTrue(await newVulnerable.thisMethodShouldReturnTrue());
+    });
+  });
 });
--- UncheckedSendCalls.test.js
+++ UncheckedSendCalls.test.js
@@ -1,57 +1,57 @@
+var Attacker = artifacts.require(
+  "/Users/alexmakarov/projs/solidity/attacks/solidity-vulnerabilities/UncheckedSendCalls.sol"
+);
 
-var Attacker = artifacts.require("/Users/alexmakarov/projs/solidity/attacks/solidity-vulnerabilities/UncheckedSendCalls.sol");
+contract("Attacker", (accounts) => {
+  var creatorAddress = accounts[0];
+  var firstOwnerAddress = accounts[1];
+  var secondOwnerAddress = accounts[2];
+  var externalAddress = accounts[3];
+  var unprivilegedAddress = accounts[4];
+  /* create named accounts for contract roles */
 
-contract('Attacker', (accounts) => {
-    var creatorAddress = accounts[0];
-    var firstOwnerAddress = accounts[1];
-    var secondOwnerAddress = accounts[2];
-    var externalAddress = accounts[3];
-    var unprivilegedAddress = accounts[4]
-    /* create named accounts for contract roles */
+  before(async () => {
+    /* before tests */
+  });
 
-    before(async () => {
-        /* before tests */
-    })
-    
-    beforeEach(async () => {
-        /* before each context */
-    })
+  beforeEach(async () => {
+    /* before each context */
+  });
 
-    it('should revert if ...', () => {
-        return Attacker.deployed()
-            .then(instance => {
-                return instance.publicOrExternalContractMethod(argument1, argument2, {from:externalAddress});
-            })
-            .then(result => {
-                assert.fail();
-            })
-            .catch(error => {
-                assert.notEqual(error.message, "assert.fail()", "Reason ...");
-            });
+  it("should revert if ...", () => {
+    return Attacker.deployed()
+      .then((instance) => {
+        return instance.publicOrExternalContractMethod(argument1, argument2, {
+          from: externalAddress,
         });
+      })
+      .then((result) => {
+        assert.fail();
+      })
+      .catch((error) => {
+        assert.notEqual(error.message, "assert.fail()", "Reason ...");
+      });
+  });
 
-    context('testgroup - security tests - description...', () => {
-        //deploy a new contract
-        before(async () => {
-            /* before tests */
-            const newAttacker =  await Attacker.new()
-        })
-        
+  context("testgroup - security tests - description...", () => {
+    //deploy a new contract
+    before(async () => {
+      /* before tests */
+      const newAttacker = await Attacker.new();
+    });
 
-        beforeEach(async () => {
-            /* before each tests */
-        })
+    beforeEach(async () => {
+      /* before each tests */
+    });
 
-        
+    it("fails on initialize ...", async () => {
+      return assertRevert(async () => {
+        await newAttacker.initialize();
+      });
+    });
 
-        it('fails on initialize ...', async () => {
-            return assertRevert(async () => {
-                await newAttacker.initialize()
-            })
-        })
-
-        it('checks if method returns true', async () => {
-            assert.isTrue(await newAttacker.thisMethodShouldReturnTrue())
-        })
-    })
+    it("checks if method returns true", async () => {
+      assert.isTrue(await newAttacker.thisMethodShouldReturnTrue());
+    });
+  });
 });
